4217ed812f9a3315b0f36df2d7cbb8cb09ee7878
Kunal Chandan <kunal_chandan@outlook.com>
2019-12-01 13:37:46 -0500

    Create firing vector, screen basics

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,18 @@ pub const WIDTH: u32 = 400;
 pub const HEIGHT: u32 = 400;
 
 // Camera Position
-pub const CAM_X: f32 = 0.;
-pub const CAM_Y: f32 = 0.;
-pub const CAM_Z: f32 = 0.;
+pub const CAM_X: f32 = 0.0;
+pub const CAM_Y: f32 = 0.0;
+pub const CAM_Z: f32 = 0.0;
 // Camera rotation
-pub const CAM_T: f32 = 0.; // Theta ( Right Left plane )
-pub const CAM_P: f32 = 0.; // Phi ( Up down plane)
+pub const CAM_T: f32 = 0.0; // Theta ( Right Left plane )
+pub const CAM_P: f32 = 0.0; // Phi ( Up down plane)
+
+pub const SCR_X: f32 = 1.0; // Right left width of screen
+pub const SCR_Y: f32 = 1.0; // Up down width of screen
+pub const SCR_Z: f32 = 0.5; // How far forwards the screen is
+
+pub const RAY_BOUNCE_MAX: u8 = 5;
 
 
 struct Ray {
@@ -33,17 +39,29 @@ struct Sphere {
     x0: f32,
     y0: f32,
     z0: f32,
-    r: f32,
+    r: f32
 }
 
 //fn ray_collision(ray: Ray, )
 
+fn create_ray(o: nl::Vector3<f32>, q: nl::Vector3<f32>, count: u8) -> Ray {
+    let d: nl::Vector3<f32> = q - o;
+    return Ray {
+        o,
+        d,
+        count
+    }
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let cam_pos: nl::Vector3<f32> = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z);
     for i in 0..WIDTH {
         for j in 0..HEIGHT {
             // Create Ray from position to screen
+            // TODO:: Factor in Camera rotation; Rotation matrix must be applied to scr_pos vector..
+            let scr_pos: nl::Vector3<f32> = nl::Vector3::new(i*SCR_X/WIDTH, j*SCR_Y/HEIGHT, SCR_Z) + cam_pos;
+            let mut r = create_ray(cam_pos, scr_pos, RAY_BOUNCE_MAX);
             for obj in set_of_objects {
                 // If ray intersects object
                     // Find normal of object at this point
