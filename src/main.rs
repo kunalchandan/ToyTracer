28eb818d37e4093d2af253aceff6858b6f21464c
Kunal Chandan <kunal_chandan@outlook.com>
2019-12-20 00:49:44 -0500

    Proper Camera Vector Calculations

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use piston_window::*;
 use crate::ray::*;
 use crate::world::*;
 use crate::display::*;
+use nl::Vector3;
 
 mod ray;
 mod world;
@@ -30,9 +31,9 @@ pub const CAM_Z: f32 = 0.0;
 pub const CAM_T: f32 = 0.0; // Theta ( Right Left plane )
 pub const CAM_P: f32 = 0.0; // Phi ( Up down plane)
 
-pub const SCR_X: f32 = 1.0; // Right left width of screen
-pub const SCR_Y: f32 = 1.0; // Up down width of screen
-pub const SCR_Z: f32 = 0.0; // How far forwards the screen is
+pub const SCR_T: f32 = 1.0; // Right left width of screen
+pub const SCR_P: f32 = 1.0; // Up down width of screen
+pub const SCR_Z: f32 = 1.0; // How far forwards the screen is
 
 
 pub struct Tracer {
@@ -54,29 +55,29 @@ impl Tracer {
     }
 
     pub fn create_world(&mut self) {
-        self.add_object( Plane {
-            a: 1.0,
-            b: 2.0,
-            c: 3.0,
-            d: 4.0
-        });
-        self.add_object(Plane {
-            a: 0.0,
-            b: 1.0,
-            c: 2.0,
-            d: 3.0
-        });
+//        self.add_object( Plane {
+//            a: 1.0,
+//            b: 2.0,
+//            c: 3.0,
+//            d: 4.0
+//        });
+//        self.add_object(Plane {
+//            a: 0.0,
+//            b: 1.0,
+//            c: 2.0,
+//            d: 3.0
+//        });
+//        self.add_object(Sphere {
+//            x0: 2.0,
+//            y0: 2.0,
+//            z0: 0.0,
+//            r: 3.0
+//        });
         self.add_object(Sphere {
-            x0: 2.0,
-            y0: 2.0,
+            x0: 5.0,
+            y0: 5.0,
             z0: 0.0,
-            r: 3.0
-        });
-        self.add_object(Sphere {
-            x0: 1.0,
-            y0: 1.0,
-            z0: 1.0,
-            r: 1.0
+            r: 2.00
         });
     }
 
@@ -98,16 +99,45 @@ impl Tracer {
     pub fn set_hit_img(&mut self) {
         let cam_pos: nl::Vector3<f32> = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z);
 
-        // Let is draw the background.
-        // self.get_background(i, j);
+        let mut z = CAM_Z + SCR_Z;
+        // Centre
+        let cn = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z) + (SCR_Z * nl::Vector3::new(CAM_P.sin() * CAM_T.cos(),
+                                                                                   CAM_P.sin() * CAM_T.sin(),
+                                                                                   CAM_P.cos()));
+        let ul = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z) +
+                (nl::Vector3::new((CAM_P - SCR_P).sin() * (CAM_T + SCR_T).cos(),
+                                  (CAM_P - SCR_P).sin() * (CAM_T + SCR_T).sin(),
+                                  (CAM_P - SCR_P).cos()) * SCR_Z);
 
+        let ur = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z) +
+                (nl::Vector3::new((CAM_P - SCR_P).sin() * (CAM_T - SCR_T).cos(),
+                                  (CAM_P - SCR_P).sin() * (CAM_T - SCR_T).sin(),
+                                  (CAM_P - SCR_P).cos()) * SCR_Z);
+
+        let bl = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z) +
+                (nl::Vector3::new((CAM_P + SCR_P).sin() * (CAM_T + SCR_T).cos(),
+                                  (CAM_P + SCR_P).sin() * (CAM_T + SCR_T).sin(),
+                                  (CAM_P + SCR_P).cos()) * SCR_Z);
+
+        let br = nl::Vector3::new(CAM_X, CAM_Y, CAM_Z) +
+                (nl::Vector3::new((CAM_P + SCR_P).sin() * (CAM_T - SCR_T).cos(),
+                                  (CAM_P + SCR_P).sin() * (CAM_T - SCR_T).sin(),
+                                  (CAM_P + SCR_P).cos()) * SCR_Z);
+        let dx = (ul[0] - br[0])/(WIDTH  as f32);
+        let dy = (ul[1] - br[1])/(WIDTH  as f32);
+        let dz = (ul[2] - br[2])/(HEIGHT as f32);
+        let v_down = (bl - ul)/(HEIGHT as f32);
+        let mut cur_y0 = ul;
+        let mut cur_y1 = ur;
         for j in 0..HEIGHT {
+            cur_y0 += v_down;
+            cur_y1 += v_down;
+            let v_right = (cur_y1 - cur_y0)/(WIDTH as f32);
+            let mut scr_pos = cur_y0;
             for i in 0..WIDTH {
+                scr_pos += v_right;
                 // Create Ray from position to screen
                 // TODO:: Factor in Camera rotation; Rotation matrix must be applied to scr_pos vector..
-                let scr_pos: nl::Vector3<f32> = nl::Vector3::new((i as f32)*SCR_X/(WIDTH as f32),
-                                                                 (j as f32)*SCR_Y/(HEIGHT as f32),
-                                                                 SCR_Z) + cam_pos;
                 let r = create_ray(cam_pos, scr_pos, RAY_BOUNCE_MAX);
                 for obj in self.objects.iter() {
                     let r_new = obj.trace(r);
@@ -165,6 +195,34 @@ fn main() {
         e.mouse_cursor(|pos| {
             // Update cursor position
         });
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            let i = 0;
+            tracer.objects[i].get_location();
+            if key == Key::Left {
+                println!("Move X-");
+                tracer.objects[i].move_xn();
+            }
+            if key == Key::Right {
+                println!("Move X+");
+                tracer.objects[i].move_xp();
+            }
+            if key == Key::Up {
+                println!("Move Y-");
+                tracer.objects[i].move_yn();
+            }
+            if key == Key::Down {
+                println!("Move Y+");
+                tracer.objects[i].move_yp();
+            }
+            if key == Key::LShift {
+                println!("Move Z-");
+                tracer.objects[i].move_zn();
+            }
+            if key == Key::LCtrl {
+                println!("Move Z+");
+                tracer.objects[i].move_zp();
+            }
+        }
 
         if let Some(r) = e.render_args() {
 
